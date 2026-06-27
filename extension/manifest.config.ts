e5361848929012702b54c6726ff957c1efb4d0ec
Jamie Yu <jamiekyu@gmail.com>
2026-06-26 17:20:20 -0700

    release: extension v0.1.2

--- extension/manifest.config.ts
+++ extension/manifest.config.ts
@@ -7,7 +7,7 @@ import { defineManifest } from '@crxjs/vite-plugin'
 export default defineManifest({
   manifest_version: 3,
   name: 'Forge for Immich',
-  version: '0.1.1',
+  version: '0.1.2',
   description: 'Forge low-quality Immich photos: denoise, upscale, face-restore, colorize.',
   // tabs/webNavigation: detect the asset id in the active tab's URL (incl. SPA
   // navigation). activeAssetId is mirrored into storage.session for the panel.
